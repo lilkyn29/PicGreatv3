b8b5450818185161d9815da77aa5ea88c28ad7d0
lilkyn29 <nguyenanhhungvuong.1203@gmail.com>
2026-04-02 01:11:35 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/PicGreatV3/',
+    base: '/PicGreatv3/',
     plugins: [react(), tailwindcss()],
     define: {},
     resolve: {
